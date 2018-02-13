5a535f9d4167519fb006dcfca595acc283962e66
Jake Atakhanov <j.atahanov@yahoo.com>
2018-02-13 04:26:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+describe('shout(string)', ()=> { 
+})
+function shout(string){
+  return string.toUpperCase() 
+  }
+describe('whisper(string)', ()=> { 
+})
+function whisper(string){
+  return string.toLowerCase() 
+  }
+describe('logShout(string)' , ()=>{
+})
+function logShout(string){ 
+console.log (string.toUpperCase())
+}
+describe ('logWhisper(string)', () => {
+})
+function logWhisper(string){
+  console.log (string.toLowerCase())
+}
+describe ('sayHiToGrandma(string)', () => {
+})
+function sayHiToGrandma(string){
+  return 'hello'.toLowerCase()
+  } 
