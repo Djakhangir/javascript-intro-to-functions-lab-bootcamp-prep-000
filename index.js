955a1cf355a238b2ee3e40e4f085fcc59b77656d
Jake Atakhanov <j.atahanov@yahoo.com>
2018-02-13 05:26:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ function logWhisper(string){
 describe('sayHiToGrandma(string)', () => {
 })
 function sayHiToGrandma(string){
-  return string.toLowerCase().toEqual
+  return hello.toLowerCase().toEqual
   } 
